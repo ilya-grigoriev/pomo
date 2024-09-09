fa119dba8d689ad6bcabaf8cbef7de6648be6e02
Ilya Grigoriev <iliagrigoryev@yandex.ru>
2024-09-09 21:08:29 +0300

    feat: added config.def.h file

---
+++ src/config.def.h
@@ -0,0 +1,13 @@
+#include "settings.h"
+
+#define LONG_PAUSE 30
+#define SHORT_PAUSE 0
+#define POMODOROS 4
+#define WORK_TIME 30
+
+int settings_int[SETTINGS_ITEMS] = {
+	LONG_PAUSE,
+	SHORT_PAUSE,
+	POMODOROS,
+	WORK_TIME,
+};
--- src/settings.h
+++ src/settings.h
@@ -8,8 +8,3 @@ extern int cur_settings_active_item;
 #define SETTINGS_LOGO_ROW 5
 extern char settings_logo[SETTINGS_LOGO_ROW][100];
 #define SETTINGS_LOGO_MARGIN_OF_CENTER 10
-
-#define LONG_PAUSE 30
-#define SHORT_PAUSE 5
-#define POMODOROS 4
-#define WORK_TIME 30
