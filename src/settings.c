16c7b919c44518bb6a74ebdbe2cd521f64782039
Ilya Grigoriev <iliagrigoryev@yandex.ru>
2024-09-11 16:09:00 +0300

    feat(settings): added action for reset button

--- src/settings.c
+++ src/settings.c
@@ -12,6 +12,12 @@ char settings_str[SETTINGS_ITEMS][30] = {
 	"Work time: <%d>",
 	"RESET",
 };
+int settings_vals[SETTINGS_ITEMS] = {
+	LONG_PAUSE,
+	SHORT_PAUSE,
+	POMODOROS,
+	WORK_TIME,
+};
 int cur_settings_active_item = 1;
 
 char settings_logo[SETTINGS_LOGO_ROW][100] = {
@@ -22,16 +28,9 @@ char settings_logo[SETTINGS_LOGO_ROW][100] = {
 	R"(|____/|_____| |_|   |_| |___|_| \_|\____|____/ )",
 };
 
-int is_error_in_check_config_data();
-
 void settings(int row, int col, WINDOW *settings_win)
 {
 	int ch;
-
-	if (is_error_in_check_config_data()) {
-		print_error("invalid config.def.h");
-		return;
-	}
 
 	print_settings_logo(row, col);
 	print_settings_window(row, col);
@@ -41,15 +40,15 @@ void settings(int row, int col, WINDOW *settings_win)
 			cur_settings_active_item += (cur_settings_active_item+1 <= SETTINGS_ITEMS) ? 1 : 0;
 		else if (ch == 'k' || ch == KEY_UP)
 			cur_settings_active_item -= (cur_settings_active_item-1 >= 1) ? 1 : 0;
+		else if ((ch == 'l' || ch == ENTER) && cur_settings_active_item == SETTINGS_ITEMS)
+				reset_settings();
 		else if (ch == 'l' || ch == KEY_RIGHT)
-			if (cur_settings_active_item != SETTINGS_ITEMS)
-				settings_int[cur_settings_active_item-1] += 1;
-			else
-				print_error("unavailable motion");
+			if (cur_settings_active_item != SETTINGS_ITEMS) // no reset button
+				settings_vals[cur_settings_active_item-1] += 1;
 		else if (ch == 'h' || ch == KEY_LEFT) 
 			if (cur_settings_active_item != SETTINGS_ITEMS)
-				if (settings_int[cur_settings_active_item-1] - 1 > 0)
-					settings_int[cur_settings_active_item-1] -= 1;
+				if (settings_vals[cur_settings_active_item-1] - 1 > 0)
+					settings_vals[cur_settings_active_item-1] -= 1;
 				else
 					print_error("min value is 1\n");
 			else
@@ -66,7 +65,16 @@ void settings(int row, int col, WINDOW *settings_win)
 	cur_settings_active_item = 1;
 }
 
-int is_error_in_check_config_data(void)
+void reset_settings(void)
 {
-	return (LONG_PAUSE < 1) || (SHORT_PAUSE < 1) || (POMODOROS < 1) || (WORK_TIME < 1);
+	int default_settings_vals[SETTINGS_ITEMS] = {
+		LONG_PAUSE,
+		SHORT_PAUSE,
+		POMODOROS,
+		WORK_TIME,
+	};
+
+	for (int i = 0; i < SETTINGS_ITEMS; i++) {
+		settings_vals[i] = default_settings_vals[i];
+	}
 }
--- src/settings.h
+++ src/settings.h
@@ -1,10 +1,14 @@
+#include <ncurses.h>
+
 void settings(int row, int col, WINDOW *settings_win);
 
 #define SETTINGS_ITEMS 5
 extern char settings_str[SETTINGS_ITEMS][30];
-extern int settings_int[SETTINGS_ITEMS];
+extern int settings_vals[SETTINGS_ITEMS];
 extern int cur_settings_active_item;
 
 #define SETTINGS_LOGO_ROW 5
 extern char settings_logo[SETTINGS_LOGO_ROW][100];
 #define SETTINGS_LOGO_MARGIN_OF_CENTER 10
+
+void reset_settings(void);
