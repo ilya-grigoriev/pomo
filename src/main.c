12fd7d4c02be5eb42727e5c00842c927d48c03d0
Ilya Grigoriev <iliagrigoryev@yandex.ru>
2024-09-11 18:11:19 +0300

    feat(main): added checker `config.def.h`

--- src/main.c
+++ src/main.c
@@ -1,16 +1,23 @@
 #include <ncurses.h>
 #include <string.h>
 #include "startpage.h"
+#include "print.h"
 
 WINDOW *create_newwin(int height, int width, int starty, int startx);
-
+int is_error_in_check_config_data(void);
 
 int main()
 {	
 	int row, col;
 
+	if (is_error_in_check_config_data()) {
+		printf("invalid config.def.h\n");
+		return -1;
+	}
+
 	initscr();			
 	curs_set(0);
+	noecho();
 	getmaxyx(stdscr,row,col);
 
 	start_color();
