ee31514565f2e39121eb16e68174cd20cf1692e0
Ilya Grigoriev <iliagrigoryev@yandex.ru>
2024-09-11 18:10:58 +0300

    fix(config.def.h)

--- src/config.def.h
+++ src/config.def.h
@@ -1,13 +1,6 @@
 #include "settings.h"
 
 #define LONG_PAUSE 30
-#define SHORT_PAUSE 0
+#define SHORT_PAUSE 1
 #define POMODOROS 4
 #define WORK_TIME 30
-
-int settings_int[SETTINGS_ITEMS] = {
-	LONG_PAUSE,
-	SHORT_PAUSE,
-	POMODOROS,
-	WORK_TIME,
-};
