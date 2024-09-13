db197f9df55a8d26739ecda4a01ef0c3cf6cd41e
Ilya Grigoriev <iliagrigoryev@yandex.ru>
2024-09-13 18:16:46 +0300

    refactor(play): moved two funcs to one

--- src/play.c
+++ src/play.c
@@ -9,64 +9,58 @@
 #include "play.h"
 #include "settings.h"
 
-int run_work_timer(int row, int col, WINDOW *play_win);
-int run_chill_timer(int row, int col, WINDOW *play_win);
+#define QUIT 0
 
-struct Play Play_T;
+struct timer Timer;
 
 void play(int row, int col, WINDOW *play_win)
 {
-	Play_T.is_stop = 0;
+	int state;
+	int pomodoros = settings_vals[POMODOROS_INDEX];
+	Timer.is_stop = 0;
 
 	nodelay(play_win, 1);
-	run_work_timer(row, col, play_win);
-	run_chill_timer(row, col, play_win);
+
+	int cur_pomodoros = 0;
+	for (int count_launches_timer = 1; count_launches_timer <= pomodoros * 2; count_launches_timer++) {
+		if (count_launches_timer % 2 != 0) {
+			Timer.type_timer = TYPE_TIMER_WORK;
+			cur_pomodoros++;
+		}
+		else if (count_launches_timer == pomodoros * 2 || count_launches_timer % 8 == 0) 
+				Timer.type_timer = TYPE_TIMER_LONG_CHILL;
+		else
+			Timer.type_timer = TYPE_TIMER_CHILL;
+
+		state = run_timer(row, col, play_win, cur_pomodoros);
+		if (state == QUIT)
+			break;
+	}
 }
 
-int run_work_timer(int row, int col, WINDOW *play_win)
+int run_timer(int row, int col, WINDOW *play_win, int pomodoros)
 {
-	Play_T.rest_time = settings_vals[0] * 60; // secs
+	Timer.rest_time = settings_vals[Timer.type_timer] * 60;
 	int ch;
 
-	print_clock(row, col, Play_T);
-	while (Play_T.rest_time > 0) {
+	print_pomodoros(row, col, pomodoros);
+	print_clock(row, col, Timer);
+	while (Timer.rest_time > 0) {
 		napms(1000);
 
 		ch = wgetch(play_win);
 		if (ch == 'q')
-			return 0;
+			return QUIT;
 		else if (ch == 's')
 			break;
 		else if (ch == 'p' || ch == ' ')
-			Play_T.is_stop = (Play_T.is_stop == 0) ? 1 : 0;
+			Timer.is_stop = (Timer.is_stop == 0) ? 1 : 0;
 
-		if (Play_T.is_stop == 0)
-			Play_T.rest_time--;
+		if (Timer.is_stop == 0)
+			Timer.rest_time--;
 
-		print_clock(row, col, Play_T);
+		print_pomodoros(row, col, pomodoros);
+		print_clock(row, col, Timer);
 	}
 }
 
-int run_chill_timer(int row, int col, WINDOW *play_win)
-{
-	Play_T.rest_time = SHORT_PAUSE * 60; // secs
-	int ch;
-
-	print_clock(row, col, Play_T);
-	while (Play_T.rest_time > 0) {
-		napms(1000);
-
-		ch = wgetch(play_win);
-		if (ch == 'q')
-			return 0;
-		else if (ch == 's')
-			break;
-		else if (ch == 'p' || ch == ' ')
-			Play_T.is_stop = (Play_T.is_stop == 0) ? 1 : 0;
-
-		if (Play_T.is_stop == 0)
-			Play_T.rest_time--;
-
-		print_clock(row, col, Play_T);
-	}
-}
--- src/play.h
+++ src/play.h
@@ -4,12 +4,18 @@
 void run_clock(int row, int col, WINDOW *play_win);
 void get_signal(void);
 void play(int row, int col, WINDOW *play_win);
+int run_timer(int, int, WINDOW*, int pomodoros);
+
+#define TYPE_TIMER_WORK 0
+#define TYPE_TIMER_CHILL 1
+#define TYPE_TIMER_LONG_CHILL 2
 
 #ifndef PLAY_H
 #define PLAY_H
-struct Play {
+struct timer {
 	int is_stop;
 	int pomodoros;
+	int type_timer; // `WORK`, `CHILL`, `LONG_CHILL`
 	clock_t rest_time;
 };
 #endif
