be7d93ba41e34d010986d4a733f76d703f016a5e
Ilya Grigoriev <iliagrigoryev@yandex.ru>
2025-02-02 14:05:20 +0300

    feat(args): added `help` flag

--- src/args.c
+++ src/args.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include "args.h"
@@ -9,5 +10,9 @@ void check_args(int argc, char *argv[])
 	if (argc == 2) {
 		if (strcmp(argv[1], "-n") == 0 || strcmp(argv[1], "--notification") == 0)
 			NOTIFICATION = 1;
+		if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
+			printf("usage: pomo [-n] [--notification] [-h] [--help]\n");
+			exit(0);
+		}
 	}
 }
